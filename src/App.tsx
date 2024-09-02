ace3937a538fbe5f23c1cf63e0980d03eb62e63f
dima.rogkov <dimarogkov41475@ukr.net>
2024-09-03 02:31:53 +0300

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export const App = () => {
     return (
         <>
             <Header />
-            <main className='relative w-full max-w-screen-2xl p-4 sm:p-5 m-auto'>
+            <main className='relative w-full max-w-screen-2xl p-4 sm:px-5 sm:py-5 lg:py-8 m-auto'>
                 <Outlet />
             </main>
         </>
--- src/components/elements/index.ts
+++
@@ -1,4 +0,0 @@
-import { Header } from './Header';
-import { StoreRedux } from './StoreRedux';
-
-export { Header, StoreRedux };
